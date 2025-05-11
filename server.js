62c2fff2b43eecce14e125bdb744ddd344f6e12e
Asif Amin <167528072+Strilitxia@users.noreply.github.com>
2025-05-11 23:48:44 +0600

    Delete server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-// server.js
-const express = require('express');
-const app = express();
-app.use(express.static('.'));
-app.listen(3000, () =>
-  console.log('Serving on http://localhost:3000')
-);
